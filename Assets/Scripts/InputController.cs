c49c8df6bc4ec712b88ae6fece7a6df80d0ac0e9
Esker_Gar <lobachevartem@gmail.com>
2020-07-08 13:52:07 +0300

    GeneratePlatform+Ball + ControllPlatform

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private const float LeftBorder = -8f;
+    private const float RightBorder = 8f;
+    
+    private Ball _ball;
+    private Platform _platform;
+    private Camera _cam;
+    [SerializeField] private PlayerGeneration playerGeneration;
+
+    private void Awake()
+    {
+        _cam = Camera.main;
+        playerGeneration.OnCreatePlatform += GetPlatform;
+        playerGeneration.OnCreateBall += GetBall;
+    }
+
+    private void GetBall(Ball ball) => _ball = ball;
+
+    private void GetPlatform(Platform platform) => _platform = platform;
+
+    private void Update()
+    {
+        SetPlatformPos(GetMousePos().x);
+        if(_platform.transform.position.x < LeftBorder)
+            SetPlatformPos(LeftBorder);
+        else if (_platform.transform.position.x > RightBorder)
+            SetPlatformPos(RightBorder);
+    }
+
+    private void SetPlatformPos(float xPos) => _platform.transform.position = new Vector3(xPos, _platform.transform.position.y);
+
+    private Vector3 GetMousePos()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        mousePos = _cam.ScreenToWorldPoint (mousePos);
+        return mousePos;
+    }
+    
+}
---
+++ Assets/Scripts/PlayerGeneration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGeneration : MonoBehaviour
+{
+    [SerializeField] private GameObject platformPrefab;
+    [SerializeField] private GameObject ballPrefab;
+
+    public event Action<Platform> OnCreatePlatform;
+    public event Action<Ball> OnCreateBall; 
+    
+    void Start()
+    {
+        var platform = CreatePlatform();
+        OnCreatePlatform?.Invoke(platform.GetComponent<Platform>());
+        var ball = BallCreate(platform);
+        OnCreateBall?.Invoke(ball.GetComponent<Ball>());
+    }
+
+    private GameObject CreatePlatform()
+    {
+        return Instantiate(platformPrefab, transform.position, Quaternion.identity);
+    }
+
+    public GameObject BallCreate(GameObject platform)
+    {
+        var platformTrans = platform.transform;
+        var BallPos = platformTrans.position + new Vector3(0, platformTrans.localScale.y / 2 + ballPrefab.transform.localScale.y / 2);
+        return Instantiate(ballPrefab, BallPos, Quaternion.identity);
+    }
+    
+}
